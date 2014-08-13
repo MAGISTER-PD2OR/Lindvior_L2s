498c1826a49e76e9b4ed5580a309cac9c48ef518
Kiệt Thành Võ <vo_thanh_kiet@yahoo.com>
2014-08-13 15:23:55 +0700

    Fix Aggression and Mentoring Cancel
    Aggression: force player attack caster
    Mentoring Cancel: remove link from database

--- blood/src/main/java/l2s/gameserver/skills/skillclasses/Aggression.java
+++ blood/src/main/java/l2s/gameserver/skills/skillclasses/Aggression.java
@@ -55,15 +55,7 @@ public class Aggression extends Skill
                     target.setTarget(activeChar);
                     // Force attack
                     int atkRange = target.getActiveWeaponTemplate().getAttackRange();
-                    double distance = target.getRealDistance(activeChar);
-                    if(distance <= atkRange)
-                    {
-                        target.doAttack(activeChar);
-                    }
-                    else
-                    {
-                        target.moveToLocation(activeChar.getLoc(),atkRange,true);
-                    }
+                    target.getAI().Attack(activeChar,true,false);
                 }
 				getEffects(activeChar, target, false);
 			}
