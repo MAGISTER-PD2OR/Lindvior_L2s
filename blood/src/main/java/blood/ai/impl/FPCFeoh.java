bcdc188f0916e3436a37558b21bdeef7b8c35650
namlehong <namlehong@gmail.com>
2014-08-13 21:57:35 +0700

    fix feoh nec debuff fail

--- blood/src/main/java/blood/ai/impl/FPCFeoh.java
+++ blood/src/main/java/blood/ai/impl/FPCFeoh.java
@@ -47,6 +47,9 @@ public class FPCFeoh extends MysticPC
 	public final int SKILL_WIND_STANCE 	= 11009;
 	public final int SKILL_EARTH_STANCE	= 11010;
 	
+	protected long _darkcureTS = 0;
+	protected int _darkcureReuse = 30000;
+	
 	public FPCFeoh(Player actor)
 	{
 		super(actor);
@@ -144,17 +147,11 @@ public class FPCFeoh extends MysticPC
 		
 		debug("prepare target:" + target);
 		
-		if (actor.getServitors().length > 0){
-			for (Servitor summon: actor.getServitors())
-			{
-				summon.getAI().Attack(target, true, false);
-			}
-		}
-		
-		
 		double distance = actor.getDistance(target);
 		double targetHp = target.getCurrentHpPercents();
 		double actorHp = actor.getCurrentHpPercents();
+		
+		long now = System.currentTimeMillis();
 		
 		Skill skillDeathCurse = actor.getKnownSkill(SKILL_DEVIL_CURSE);
 		Skill skillElementBurst = actor.getKnownSkill(SKILL_ELEMENT_BURST_DE);
@@ -170,8 +167,11 @@ public class FPCFeoh extends MysticPC
 			chooseTaskAndTargets(skillDeathFear, target, distance);
 		
 		// if we are feoh soul taker we should debuf darkcurse first
-		if(canUseSkill(skillDarkCurse, target, distance) && target.getEffectList().getEffectsCount(skillDarkCurse) == 0)
+		if((now - _darkcureTS) > _darkcureReuse && canUseSkill(skillDarkCurse, target, distance) && target.getEffectList().getEffectsCount(skillDarkCurse) == 0){
+			_darkcureTS = now;
 			return chooseTaskAndTargets(skillDarkCurse, target, distance);
+		}
+			
 		
 		// 1st use death curse
 		if(canUseSkill(skillDeathCurse, target, distance) && target.getEffectList().getEffectsCount(skillDeathCurse) == 0)
@@ -204,64 +204,7 @@ public class FPCFeoh extends MysticPC
 		if(canUseSkill(skillElementDestruction, target, distance))
 			return chooseTaskAndTargets(skillElementDestruction, target, distance);
 			
-		Skill[] dam = Rnd.chance(getRateDAM()) ? selectUsableSkills(target, distance, _damSkills) : null;
-		Skill[] dot = Rnd.chance(getRateDOT()) ? selectUsableSkills(target, distance, _dotSkills) : null;
-		Skill[] debuff = targetHp > 10 ? Rnd.chance(getRateDEBUFF()) ? selectUsableSkills(target, distance, _debuffSkills) : null : null;
-		Skill[] stun = Rnd.chance(getRateSTUN()) ? selectUsableSkills(target, distance, _stunSkills) : null;
-		Skill[] heal = actorHp < 50 ? Rnd.chance(getRateHEAL()) ? selectUsableSkills(actor, 0, _healSkills) : null : null;
-		Skill[] buff = Rnd.chance(getRateBUFF()) ? selectUsableSkills(actor, 0, _buffSkills) : null;
-		
-		RndSelector<Skill[]> rnd = new RndSelector<Skill[]>();
-		if (!actor.isAMuted())
-		{
-			rnd.add(null, getRatePHYS());
-		}
-		
-		if(dam != null && dam.length > 0)
-			rnd.add(dam, getRateDAM());
-		
-		if(dot != null && dot.length > 0)
-		rnd.add(dot, getRateDOT());
-		
-		if(debuff != null && debuff.length > 0)
-		rnd.add(debuff, getRateDEBUFF());
-		
-		if(heal != null && heal.length > 0)
-		rnd.add(heal, getRateHEAL());
-		
-		if(buff != null && buff.length > 0)
-		rnd.add(buff, getRateBUFF());
-		
-		if(stun != null && stun.length > 0)
-		rnd.add(stun, getRateSTUN());
-		
-		Skill[] selected = rnd.select();
-		if (selected != null)
-		{
-			if ((selected == dam) || (selected == dot))
-			{
-				return chooseTaskAndTargets(selectTopSkillByDamage(actor, target, distance, selected), target, distance);
-			}
-			
-			if ((selected == debuff) || (selected == stun))
-			{
-				return chooseTaskAndTargets(selectTopSkillByDebuff(actor, target, distance, selected), target, distance);
-			}
-			
-			if (selected == buff)
-			{
-				return chooseTaskAndTargets(selectTopSkillByBuff(actor, selected), actor, distance);
-			}
-			
-			if (selected == heal)
-			{
-				return chooseTaskAndTargets(selectTopSkillByHeal(actor, selected), actor, distance);
-			}
-		}
-		
-		// TODO make treatment and buff friendly targets
-		
-		return chooseTaskAndTargets(null, target, distance);
+		return false;
 	}
 	
 	
