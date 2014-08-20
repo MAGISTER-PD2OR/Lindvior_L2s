6d5b7394135b2091fbb1db6c9f6e37c767e2d56b
namlehong <namlehong@gmail.com>
2014-08-20 14:02:06 +0700

    improve name <> class

--- blood/src/main/java/blood/dao/FakeNameDAO.java
+++ blood/src/main/java/blood/dao/FakeNameDAO.java
@@ -35,37 +35,67 @@ public class FakeNameDAO {
 	
 	public static boolean isHumanName(String name)
 	{
-		return name.toLowerCase().contains("human");
+		return name.toLowerCase().contains("human")
+				|| isWarlordName(name)
+				|| isGladiatorName(name)
+				|| isPaladinName(name)
+				|| isDarkAvengerName(name)
+				|| isTreasureHunterName(name)
+				|| isHawkeyeName(name)
+				|| isSorcererName(name)
+				|| isNecromancerName(name)
+				|| isWarlockName(name)
+				|| isBishopName(name)
+				|| isProphetName(name);
 	}
 	
 	public static boolean isElfName(String name)
 	{
 		return (name.toLowerCase().contains("elf") && !name.toLowerCase().contains("darkelf"))
-				|| isSingerName(name);
+				|| isTempleKnightName(name)
+				|| isSingerName(name)
+				|| isPlainsWalkerName(name)
+				|| isSilverRangerName(name)
+				|| isSpellSingerName(name)
+				|| isElementalSummonerName(name)
+				|| isElvenElderName(name);
 	}
 	
 	public static boolean isDarkElfName(String name)
 	{
 		return name.toLowerCase().contains("darkelf")
-				|| isDancerName(name);
+				|| isShillenKnightName(name)
+				|| isDancerName(name)
+				|| isAbyssWalkerName(name)
+				|| isPhantomRangerName(name)
+				|| isSpellhowlerName(name)
+				|| isPhantomSummonerName(name)
+				|| isShillienElderName(name);
+	}
+	
+	public static boolean isOrcName(String name)
+	{
+		return name.toLowerCase().contains("orc")
+				|| isDestroyerName(name)
+				|| isTyrantName(name)
+				|| isOverlordName(name)
+				|| isWarcryerName(name);
 	}
 	
 	public static boolean isDwarfName(String name)
 	{
 		return name.toLowerCase().contains("lun") 
 				|| name.toLowerCase().contains("dwarf")
-				|| isSmithName(name)
-				|| isSpoilerName(name);
+				|| isWarSmithName(name)
+				|| isBountyHunterName(name);
 	}
 	
 	public static boolean isKamaelName(String name)
 	{
-		return name.toLowerCase().contains("kamael");
-	}
-	
-	public static boolean isOrcName(String name)
-	{
-		return name.toLowerCase().contains("orc");
+		return name.toLowerCase().contains("kamael")
+				|| isBerserkerName(name)
+				|| isSoulBreakerName(name)
+				|| isTricksterName(name);
 	}
 	
 	// sex by name
@@ -77,7 +107,9 @@ public class FakeNameDAO {
 	
 	public static boolean isFemaleName(String name)
 	{
-		return name.toLowerCase().contains("female");
+		return name.toLowerCase().contains("female")
+				|| name.toLowerCase().contains("ngoc")
+				|| name.toLowerCase().contains("linh");
 	}
 	
 	// class by name	
@@ -99,26 +131,25 @@ public class FakeNameDAO {
 				|| isDancerName(name)
 				|| isSingerName(name);
 	}
-	
-	public static boolean isSmithName(String name)
-	{
-		return name.toLowerCase().contains("che")
-				|| name.toLowerCase().contains("tao");
-	}
-	
-	public static boolean isSpoilerName(String name)
-	{
-		return name.toLowerCase().contains("spoil");
-	}
 		
 	public static boolean isTankerName(String name)
 	{
-		return name.toLowerCase().contains("tank");
+		return name.toLowerCase().contains("tank")
+				|| isPaladinName(name)
+				|| isDarkAvengerName(name)
+				|| isTempleKnightName(name)
+				|| isShillenKnightName(name);
 	}
 	
 	public static boolean isWarriorName(String name)
 	{
-		return name.toLowerCase().contains("warrior");
+		return name.toLowerCase().contains("warrior")
+				|| isWarlordName(name)
+				|| isGladiatorName(name)
+				|| isDestroyerName(name)
+				|| isTyrantName(name)
+				|| isWarSmithName(name)
+				|| isBerserkerName(name);
 	}
 	
 	public static boolean isDaggerName(String name)
@@ -126,31 +157,50 @@ public class FakeNameDAO {
 		return name.toLowerCase().contains("dagger")
 				|| name.toLowerCase().contains("dao")
 				|| name.toLowerCase().contains("assasin")
-				|| name.toLowerCase().contains("satthu");
+				|| name.toLowerCase().contains("satthu")
+				|| isTreasureHunterName(name)
+				|| isPlainsWalkerName(name)
+				|| isAbyssWalkerName(name)
+				|| isBountyHunterName(name);
 	}
 	
 	public static boolean isRangerName(String name)
 	{
 		return name.toLowerCase().contains("ranger")
 				|| name.toLowerCase().contains("bow")
-				|| name.toLowerCase().contains("cung");
+				|| name.toLowerCase().contains("cung")
+				|| isHawkeyeName(name)
+				|| isSilverRangerName(name)
+				|| isPhantomRangerName(name)
+				|| isTricksterName(name);
 	}
 	
 	public static boolean isNukerName(String name)
 	{
 		return name.toLowerCase().contains("nuker")
 				|| name.toLowerCase().contains("phep")
-				|| name.toLowerCase().contains("magic");
+				|| name.toLowerCase().contains("magic")
+				|| isSorcererName(name)
+				|| isNecromancerName(name)
+				|| isSpellSingerName(name)
+				|| isSpellhowlerName(name)
+				|| isSoulBreakerName(name);
 	}
 	
 	public static boolean isSummonerName(String name)
 	{
-		return name.toLowerCase().contains("sum");
+		return name.toLowerCase().contains("sum")
+				|| isElementalSummonerName(name)
+				|| isWarlockName(name)
+				|| isPhantomSummonerName(name);
 	}
 	
 	public static boolean isHealerName(String name)
 	{
-		return name.toLowerCase().contains("heal");
+		return name.toLowerCase().contains("heal")
+				|| isBishopName(name)
+				|| isElvenElderName(name)
+				|| isShillienElderName(name);
 	}
 	
 	public static boolean isBufferName(String name)
@@ -159,7 +209,76 @@ public class FakeNameDAO {
 				|| name.toLowerCase().contains("buff")
 				|| name.toLowerCase().contains("iss")
 				|| isSingerName(name)
-				|| isDancerName(name);
+				|| isDancerName(name)
+				|| isProphetName(name)
+				|| isOverlordName(name)
+				|| isWarcryerName(name);
+	}
+	
+	// specific class name
+	public static boolean isWarlordName(String name)
+	{
+		return name.toLowerCase().contains("warlord")
+				|| name.toLowerCase().contains("wl");
+	}
+	
+	public static boolean isGladiatorName(String name)
+	{
+		return name.toLowerCase().contains("gla");
+	}
+	
+	public static boolean isPaladinName(String name)
+	{
+		return name.toLowerCase().contains("pa");
+	}
+	
+	public static boolean isDarkAvengerName(String name)
+	{
+		return name.toLowerCase().contains("darkavanger")
+				|| name.toLowerCase().contains("da");
+	}
+	
+	public static boolean isTreasureHunterName(String name)
+	{
+		return name.toLowerCase().contains("th");
+	}
+	
+	public static boolean isHawkeyeName(String name)
+	{
+		return name.toLowerCase().contains("he");
+	}
+	
+	public static boolean isSorcererName(String name)
+	{
+		return name.toLowerCase().contains("sorc");
+	}
+	
+	public static boolean isNecromancerName(String name)
+	{
+		return name.toLowerCase().contains("nec");
+	}
+	
+	public static boolean isWarlockName(String name)
+	{
+		return name.toLowerCase().contains("meo")
+				|| name.toLowerCase().contains("cat");
+	}
+	
+	public static boolean isBishopName(String name)
+	{
+		return name.toLowerCase().contains("bishop")
+				|| name.toLowerCase().contains("bs");
+	}
+	
+	public static boolean isProphetName(String name)
+	{
+		return name.toLowerCase().contains("prophet")
+				|| name.toLowerCase().contains("pp");
+	}
+	
+	public static boolean isTempleKnightName(String name)
+	{
+		return name.toLowerCase().contains("tk");
 	}
 	
 	public static boolean isSingerName(String name)
@@ -168,11 +287,120 @@ public class FakeNameDAO {
 				|| name.toLowerCase().contains("song");
 	}
 	
+	public static boolean isPlainsWalkerName(String name)
+	{
+		return name.toLowerCase().contains("pw");
+	}
+	
+	public static boolean isSilverRangerName(String name)
+	{
+		return name.toLowerCase().contains("sr");
+	}
+	
+	public static boolean isSpellSingerName(String name)
+	{
+		return false;
+	}
+	
+	public static boolean isElementalSummonerName(String name)
+	{
+		return name.toLowerCase().contains("ngua");
+	}
+	
+	public static boolean isElvenElderName(String name)
+	{
+		return name.toLowerCase().contains("ee");
+	}
+	
+	public static boolean isShillenKnightName(String name)
+	{
+		return name.toLowerCase().contains("sk");
+	}
+	
 	public static boolean isDancerName(String name)
 	{
 		return name.toLowerCase().contains("dance")
 				|| name.toLowerCase().contains("nhay");
 	}
+	
+	public static boolean isAbyssWalkerName(String name)
+	{
+		return name.toLowerCase().contains("aw");
+	}
+	
+	public static boolean isPhantomRangerName(String name)
+	{
+		return name.toLowerCase().contains("pr");
+	}
+	
+	public static boolean isSpellhowlerName(String name)
+	{
+		return name.toLowerCase().contains("sh");
+	}
+	
+	public static boolean isPhantomSummonerName(String name)
+	{
+		return false;
+	}
+	
+	public static boolean isShillienElderName(String name)
+	{
+		return name.toLowerCase().contains("se");
+	}
+	
+	public static boolean isDestroyerName(String name)
+	{
+		return name.toLowerCase().contains("des");
+	}
+	
+	public static boolean isTyrantName(String name)
+	{
+		return name.toLowerCase().contains("tyr");
+	}
+	
+	public static boolean isOverlordName(String name)
+	{
+		return name.toLowerCase().contains("ol");
+	}
+	
+	public static boolean isWarcryerName(String name)
+	{
+		return name.toLowerCase().contains("wc");
+	}
+	
+	public static boolean isWarSmithName(String name)
+	{
+		return name.toLowerCase().contains("che")
+				|| name.toLowerCase().contains("tao")
+				|| name.toLowerCase().contains("craft");
+	}
+	
+	public static boolean isBountyHunterName(String name)
+	{
+		return name.toLowerCase().contains("spoil")
+				|| name.toLowerCase().contains("bh");
+	}
+	
+	public static boolean isBerserkerName(String name)
+	{
+		return name.toLowerCase().contains("ber");
+	}
+	
+	public static boolean isSoulBreakerName(String name)
+	{
+		return name.toLowerCase().contains("soul");
+	}
+	
+	public static boolean isTricksterName(String name)
+	{
+		return false;
+	}
+	
+	public static boolean isName(String name)
+	{
+		return false;
+	}
+	
 	
 	public String getName()
 	{
@@ -215,15 +443,15 @@ public class FakeNameDAO {
 		try
 		{
 			con = DatabaseFactory.getInstance().getConnection();
-			statement = con.prepareStatement("UPDATE fake_name set is_used = ? WHERE char_name = ?");
+			statement = con.prepareStatement("UPDATE fake_name SET is_used = ? WHERE char_name = ?");
 			statement.setInt(1, 1);
 			statement.setString(2, char_name);
-			statement.executeQuery();
+			statement.execute();
 			
 		}
 		catch(final Exception e)
 		{
-			_log.error("", e);
+			_log.error("error on query ad update used name:"+char_name, e);
 		}
 		finally
 		{
