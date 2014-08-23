fc7ff6c0708e514d6e711545b569290784ebc902
namlehong <namlehong@gmail.com>
2014-08-23 23:51:42 +0700

    debug distance

--- blood/src/main/java/blood/ai/FPCDefaultAI.java
+++ blood/src/main/java/blood/ai/FPCDefaultAI.java
@@ -979,6 +979,8 @@ public class FPCDefaultAI extends PlayerAI
 			return false;
 		}
 		
+		debug("pass check hate");
+		
 		return canAttackCharacter(target);
 	}
 	
@@ -1223,7 +1225,7 @@ public class FPCDefaultAI extends PlayerAI
 				if (!checkTarget(target, castRange))
 				{
 					debug("check range failed");
-					debug("Distance: " + actor.getRealDistance3D(target) + " castRange: " + castRange);
+					debug("Distance: " + actor.getRealDistance3D(target) + " castRange: " + castRange+" isInRange:"+actor.isInRangeZ(target, castRange));
 					actor.moveToLocation(target.getLoc(), castRange + 20, true);
 					return true;
 				}
