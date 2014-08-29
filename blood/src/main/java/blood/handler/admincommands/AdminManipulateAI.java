1e7805c9d728d702178331526fadb0099203564e
namlehong <namlehong@gmail.com>
2014-08-30 00:42:48 +0700

    add more debug

--- blood/src/main/java/blood/handler/admincommands/AdminManipulateAI.java
+++ blood/src/main/java/blood/handler/admincommands/AdminManipulateAI.java
@@ -60,6 +60,7 @@ public class AdminManipulateAI implements IAdminCommandHandler
 				break;
 				
 			case admin_dump_skills:
+				System.out.println("======= Start Skill list of "+activeChar.getClassId()+" =======");
 				for(Skill skill: activeChar.getAllSkillsArray())
 				{
 					if(skill.isPassive())
@@ -67,6 +68,7 @@ public class AdminManipulateAI implements IAdminCommandHandler
 					String niceName = skill.getName().toUpperCase().replace(" ", "_").replace("'", "");
 					System.out.println("SKILL_"+niceName+" = "+skill.getId()+",");
 				}
+				System.out.println("======= End Skill list of "+activeChar.getClassId()+" =======");
 			
 		}
 		return true;
