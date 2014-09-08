5e6b631ceb2da68f60c302cc2741ac3f4699ca48
namlehong <namlehong@gmail.com>
2014-09-08 16:47:40 +0700

    add debug msg

--- blood/src/main/java/blood/data/holder/FPItemHolder.java
+++ blood/src/main/java/blood/data/holder/FPItemHolder.java
@@ -53,16 +53,20 @@ public final class FPItemHolder extends AbstractHolder
 			for(FPRewardList reward_list: _rewards.values())
 			{
 				if(reward_list.isValidLevel(player.getLevel()))
+				{
 					System.out.println(String.format("List:%s min:%d max:%d pass levelcheck", reward_list._id, reward_list.getMinLevel(), reward_list.getMaxLevel()));
+					
+					if(reward_list.isValidClassId(player))
+						System.out.println(String.format("List:%s pass class check", reward_list._id));
+					
+					if(reward_list.isValidType(player))
+						System.out.println(String.format("List:%s pass class type", reward_list._id));
+					
+					if(reward_list.isValidType2(player))
+						System.out.println(String.format("List:%s pass class type2", reward_list._id));
+				}
 				
-				if(reward_list.isValidClassId(player))
-					System.out.println(String.format("List:%s pass class check"));
 				
-				if(reward_list.isValidType(player))
-					System.out.println(String.format("List:%s pass class type"));
-				
-				if(reward_list.isValidType2(player))
-					System.out.println(String.format("List:%s pass class type2"));
 					
 				
 //				System.out.println(String.format("List:%s min:%d max:%d id.size:%d type.size:%d type2.size:%d", 
